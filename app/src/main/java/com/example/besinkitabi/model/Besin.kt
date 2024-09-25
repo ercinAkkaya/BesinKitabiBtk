fdbcb7c82f4bedbdee3e4822d2611e3e6500dbe6
Ali Erçin Akkaya <ercinakkaya3460@gmail.com>
2024-09-26 00:11:27 +0300

    initial commit

---
+++ app/src/main/java/com/example/besinkitabi/model/Besin.kt
@@ -0,0 +1,18 @@
+package com.example.besinkitabi.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Besin(
+    @SerializedName("isim")
+    val besinIsim: String?,
+    @SerializedName("kalori")
+    val besinKalori: String?,
+    @SerializedName("karbonhidrat")
+    val besinKarbonhidrat: String?,
+    @SerializedName("protein")
+    val besinProtein: String?,
+    @SerializedName("yag")
+    val besinYag: String?,
+    @SerializedName("gorsel")
+    val besinGorsel: String?
+)
